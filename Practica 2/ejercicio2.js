e565dd5056ed41b337c8ace53738f64ea61b3f36
26-ale-20 <131723725+26-ale-20@users.noreply.github.com>
2025-05-20 20:13:05 -0600

    primera parte de destructuracion

---
+++ Practica 2/ejercicio2.js
@@ -0,0 +1,11 @@
+const persona = { 
+    nombre: "Veronica Alejandra" ,
+    edad: 21,
+    direccion: {
+        ciudad: "Qro",
+        pais: "MX"
+    }
+};
+
+// Destructuracion aqui 
+const personadatos = {nombre, edad, direccion}  
